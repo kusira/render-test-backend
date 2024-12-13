f2fcf7194fd417d8569d8843a6d00f5a0628f302
kusira <takaaki200302@gmail.com>
2024-12-13 22:52:30 +0900

    bb

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # CORS の設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://render-test-frontend-73439j1h0-kusiras-projects.vercel.app"],  # フロントエンドの URL
+    allow_origins=["http://localhost:3000", "https://render-test-frontend.vercel.app/"],  # フロントエンドの URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
